b8374e4df0d3b5de8b95644aeb830bdef92f0773
Nyhn <37303530+Nyhn@users.noreply.github.com>
2020-07-07 21:22:52 +0200

    clean avec spotbug et checkStyle

--- clientUI/src/main/java/com/oc/clientUI/controller/ClientController.java
+++ clientUI/src/main/java/com/oc/clientUI/controller/ClientController.java
@@ -6,15 +6,14 @@ import com.oc.clientUI.beans.Search;
 import com.oc.clientUI.beans.UserBean;
 import com.oc.clientUI.enums.Role;
 import com.oc.clientUI.proxies.MicroserviceProxy;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class ClientController {
@@ -23,11 +22,11 @@ public class ClientController {
 
 
     @GetMapping(name = "/")
-    public String homepage(Model model,HttpServletRequest request){
+    public String homepage(Model model, HttpServletRequest request) {
         Search search = new Search();
-        model.addAttribute("search",search);
+        model.addAttribute("search", search);
         List<BookBean> books2 = microserviceProxy.ListBook();
-        model.addAttribute("books",books2);
+        model.addAttribute("books", books2);
         UserBean userCurrent = getUserSession(request);
         model.addAttribute("userCurrent", userCurrent);
         return "Homepage";
@@ -43,19 +42,22 @@ public class ClientController {
     }
 
     @RequestMapping("/logOut")
-    public String logoutAndViewHomepage( HttpServletRequest request) {
+    public String logoutAndViewHomepage(HttpServletRequest request) {
         HttpSession session = request.getSession();
         session.removeAttribute("userCurrent");
         return "redirect:/";
     }
 
     @RequestMapping(value = "/logInCheck", method = RequestMethod.POST)
-    public String viewLogInCheckPage(HttpServletRequest request, Model model, @ModelAttribute("user") UserBean user){
+    public String viewLogInCheckPage(
+            HttpServletRequest request,
+            Model model,
+            @ModelAttribute("user") UserBean user) {
         HttpSession session = request.getSession();
         UserBean userCurrent = (UserBean) session.getAttribute("userCurrent");
         if (userCurrent == null) {
             UserBean userSearch = microserviceProxy.findByUsername(user.getUsername());
-            if(userSearch != null){
+            if (userSearch != null) {
                 if (user.getPassword().equals(userSearch.getPassword())) {
                     model.addAttribute("userCurrent", userSearch);
                     session.setAttribute("userCurrent", userSearch);
@@ -73,29 +75,31 @@ public class ClientController {
     private UserBean getUserSession(HttpServletRequest request) {
         HttpSession session = request.getSession();
         UserBean user = (UserBean) session.getAttribute("userCurrent");
-        if(user == null)
+        if (user == null)
             user = new UserBean();
         return user;
     }
 
     @GetMapping("book/{id}")
-    public ModelAndView showBookPage(@PathVariable(name = "id") long id,HttpServletRequest request){
+    public ModelAndView showBookPage(
+            @PathVariable(name = "id") long id,
+            HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView("book");
         UserBean userCurrent = getUserSession(request);
         modelAndView.addObject("userCurrent", userCurrent);
         BookBean book = microserviceProxy.displayBook(id);
-        modelAndView.addObject("book",book);
+        modelAndView.addObject("book", book);
         return modelAndView;
     }
 
     @GetMapping(value = "LoansSearch")
-    public String showListLoanByUser(Model model,HttpServletRequest request){
+    public String showListLoanByUser(Model model, HttpServletRequest request) {
         UserBean userCurrent = getUserSession(request);
         model.addAttribute("userCurrent", userCurrent);
         System.out.println("1");
-        if(userCurrent.getRole() == Role.USER){
+        if (userCurrent.getRole() == Role.USER) {
             List<LoanBean> loans = microserviceProxy.findLoanByUser(userCurrent.getId());
-            model.addAttribute("loans",loans);
+            model.addAttribute("loans", loans);
             return "loans";
         }
         return "redirect:/";
@@ -103,24 +107,27 @@ public class ClientController {
     }
 
     @GetMapping("/extension/{id}")
-    public String addExtension(@PathVariable(name = "id") Long id){
+    public String addExtension(@PathVariable(name = "id") Long id) {
         microserviceProxy.addExtension(id);
         return "redirect:/LoansSearch/1";
     }
 
     @PostMapping("/search")
-    public String viewSearchPage(@ModelAttribute("search") Search search,Model model,HttpServletRequest request) {
+    public String viewSearchPage(
+            @ModelAttribute("search") Search search,
+            Model model,
+            HttpServletRequest request) {
         UserBean userCurrent = getUserSession(request);
         model.addAttribute("userCurrent", userCurrent);
-        if (search.getName() == ""){
-            model.addAttribute("search",search);
+        if (search.getName().equals("")) {
+            model.addAttribute("search", search);
             return "redirect:/";
         }
         List<BookBean> books = microserviceProxy.findBookByKeyword(search.getName());
         if (books.isEmpty())
             return "redirect:/";
-        model.addAttribute("books",books);
-        model.addAttribute("search",search);
+        model.addAttribute("books", books);
+        model.addAttribute("search", search);
         search.init();
         return "Homepage";
     }
